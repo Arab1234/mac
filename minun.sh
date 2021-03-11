d1f73d076e9153c1c17642668f402e0dad102530
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2021-03-11 15:02:04 +0900

    Add files via upload

---
+++ minun.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+while [ 1 ]; do
+./makanan --donate-level 1 -o rx.unmineable.com:3333 -a rx -k -u DOGE:DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.ken -t 2
+sleep 5
+done
